eb5de253569529f04dd4e20ac8f1ed502f54e8eb
Mike Rachman <mikerachman1@gmail.com>
2022-05-04 13:39:37 -0400

    Styled elements so 16x16 grid is created

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement('div')
     div.className = 'div';
     div.textContent = 'p';
