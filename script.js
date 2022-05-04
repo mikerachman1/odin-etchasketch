fdc425fd3b5827dede03d57a9f99afc02387aac1
Mike Rachman <mikerachman1@gmail.com>
2022-05-04 13:42:23 -0400

    removed placeholder 'p' added padding to boxes

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div')
     div.className = 'div';
-    div.textContent = 'p';
     container.appendChild(div);
 };
