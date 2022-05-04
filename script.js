ea5971d44d90f46d874cc80e2c692858906a41e2
Mike Rachman <mikerachman1@gmail.com>
2022-05-04 14:00:23 -0400

    Added eventlistener for changing color of divs when mouseenters div

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ for (let i = 0; i < 256; i++) {
     const div = document.createElement('div')
     div.className = 'div';
     container.appendChild(div);
+    const changeColor = () => {
+        div.style.backgroundColor = 'black';
+    }
+    div.addEventListener('mouseenter', changeColor);
 };
+
+
+
