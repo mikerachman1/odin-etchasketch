edc0a0e5b37fca821806a8f3da5ed1a511fb1404
Mike Rachman <mikerachman1@gmail.com>
2022-05-04 14:59:36 -0400

    Refactored css to use grid layout insted of float

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div')
-    div.className = 'div';
-    container.appendChild(div);
-    const changeColor = () => {
-        div.style.backgroundColor = 'black';
-    }
-    div.addEventListener('mouseenter', changeColor);
+//creates 16x16 grid and sets colorchange UI
+let sketchPad = () => {
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div')
+        div.className = 'div';
+        container.appendChild(div);
+        const changeColor = () => {
+            div.style.backgroundColor = 'black';
+        }
+        div.addEventListener('mouseenter', changeColor);
+    };
 };
 
+sketchPad();
+
+//this function is to be used to allow the user to set the size of the grid
+const changeSize = (num) => {
+    for (let i = 0; i < (num * num); i++) {
+        const div = document.createElement('div')
+        div.className = 'div';
+        container.appendChild(div);
+        const changeColor = () => {
+            div.style.backgroundColor = 'black';
+        }
+        div.addEventListener('mouseenter', changeColor);
+    };
+}
 
 
+
+const sizeBtn = document.querySelector('#sizeBtn');
+//sizeBtn.addEventListener('click', )
